b6cef353c2cd185c64072cc20b5e0e2c40f026ae
root <root@Tony-Thinkpad.localdomain>
2017-08-06 13:01:50 +1000

    added google speech api

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express')
 const path = require('path')
-
+const express = require('express')
 const app = express()
 
+const multer  = require('multer')
+const upload = multer()
+
+const Speech = require('@google-cloud/speech');
+const speech = Speech();
+
 app.use(express.static(path.join(__dirname, 'build')))
+
+app.post('/api/transcribe', upload.single('file'), function (req, res) {
+	syncRecognize(req.file.buffer.toString('base64'), res)
+  res.send("Hello")
+})
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname,'/build/index.html'))
@@ -14,3 +24,47 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
   console.log('Press Ctrl+C to quit.')
 })
+
+function syncRecognize(content, res) {
+  const Speech = require('@google-cloud/speech');
+
+  const speech = Speech();
+  const encoding = 'LINEAR16';
+  const sampleRateHertz = 16000;
+  const languageCode = 'en-US';
+
+  const config = {
+  	enableWordTimeOffsets: true,
+    encoding: encoding,
+  	sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode
+  };
+  const audio = {
+    content: content
+  };
+
+  const request = {
+    config: config,
+    audio: audio
+  };
+
+  console.log("Text sent")
+  speech.recognize(request)
+    .then((results) => {
+      const transcription = results[0].results[0].alternatives[0].transcript;
+      console.log(`Transcription: `, transcription);
+      results[0].results[0].alternatives[0].words.forEach((wordInfo) => {
+        // NOTE: If you have a time offset exceeding 2^32 seconds, use the
+        // wordInfo.{x}Time.seconds.high to calculate seconds.
+        const startSecs = `${wordInfo.startTime.seconds}` + `.` +
+            (wordInfo.startTime.nanos / 100000000);
+        const endSecs = `${wordInfo.endTime.seconds}` + `.` +
+            (wordInfo.endTime.nanos / 100000000);
+        console.log(`Word: ${wordInfo.word}`);
+        console.log(`\t ${startSecs} secs - ${endSecs} secs`);
+      });
+    })
+    .catch((err) => {
+      console.error('ERROR:', err);
+    })
+}
--- src/App.js
+++ src/App.js
@@ -3,16 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    file: null,
+    audioSrc: ""
+  }
+
+  handleFileAdd = e => {
+    if (e.target.files.length > 0) {
+      this.setState({file: e.target.files[0]})
+    }
+  }
+
+  handleUpload = e => {
+    e.preventDefault()
+    if (this.state.file) {
+      this.setState({audioSrc: URL.createObjectURL(this.state.file)})
+      var data = new FormData()
+      data.append('file', this.state.file)
+
+      fetch('/api/transcribe', {
+        method: 'POST',
+        body: data
+      }).then(e => console.log(e))
+      // const reader = new FileReader()
+      // reader.readAsArrayBuffer(this.state.file)
+      // reader.onload = e => {
+      //   const audioContext = new AudioContext()
+      //   audioContext.decodeAudioData(reader.result).then(decodedBuffer => {
+      //     console.log(decodedBuffer)
+      //   })
+      // }
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleUpload}>
+          <input
+            type="file"
+            name="audio"
+            accept="audio/*"
+            onChange={this.handleFileAdd}
+          />
+          <button disabled={!this.state.file}>
+            Upload
+          </button>
+        </form>
+        <audio
+          controls
+          src={this.state.audioSrc}>
+          Browser does not support audio
+        </audio>
       </div>
     );
   }
